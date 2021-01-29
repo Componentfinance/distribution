a1abb9b24c7f0e2c9ebbe70ba7ceca62688eae51
34x4p08 <34x4p08@gmail.com>
2021-01-29 22:10:26 +0300

    Add background

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import styled from 'styled-components'
 
 import { Box, Grommet, Heading } from "grommet";
 import { grommet } from "grommet/themes";
 import { Distribution } from "./components/Distribution";
 import { Head } from "./components/Head";
+import "normalize.css/normalize.css";
 
+
+const Styled = styled.div`
+  background: radial-gradient(circle at top, #fff -0%, rgb(242, 193, 241), rgba(203, 0, 255, 0.18));
+  min-height: 100vh;
+`
 
 class App extends Component {
     render() {
         return (
             <Grommet theme={grommet}>
-                <Head />
-                <Box align="center" pad={{ bottom:"large" }}>
-                    <Heading level="3">Realtime Component LP rewards distribution</Heading>
-                </Box>
-                <Distribution />
+                <Styled>
+                    <Head />
+                    <Box align="center" pad={{ bottom:"large" }}>
+                        <Heading level="3">Realtime Component LP rewards distribution</Heading>
+                    </Box>
+                    <Distribution />
+                </Styled>
             </Grommet>
         );
     }
